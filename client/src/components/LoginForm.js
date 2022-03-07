293123ace60a6cf6508479c1eb23f6ab7c1c1218
keithlyang <keithluckiyang@gmail.com>
2022-03-07 15:27:16 -0600

    trying to debug

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/client';
-import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -38,7 +38,6 @@ const LoginForm = () => {
     try {
       const { data } = await login({ variables: { ...userFormData }, });
 
-      
       console.log(data);
       Auth.login(data.login.token);
     } catch (err) {
